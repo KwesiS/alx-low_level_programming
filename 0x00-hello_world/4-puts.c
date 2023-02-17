5b5cd3886a65d0fcbde0a564914235a71e207b9b
Sheriff <acorlor2real@gmail.com>
2023-02-16 17:50:07 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-
+/** 
+ * main- use puts to print the string intead of printf
+ * Return: ends with 0
+*/
 int main(void)
-{
-      puts("Programming is like buiding a multilingual puzzle\n");
 
-      return(0);
+{
+      puts("\"Programming is like buiding a multilingual puzzle");
+
+return (0);
 
 }

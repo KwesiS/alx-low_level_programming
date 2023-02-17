7e1e064ad8179cf5f06db406bc4d489d21d73285
Sheriff <acorlor2real@gmail.com>
2023-02-17 05:29:39 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
  *main - A program that prints a line followed by a new line
  *Return: 0 (Success)
 */
- int main(void)
+int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
+return (1);
 }

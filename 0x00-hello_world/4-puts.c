d2ded84da090c6c5b29f1cee2309cd66cef4aaa4
Sheriff <acorlor2real@gmail.com>
2023-02-16 23:38:52 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/** 
- * main- A program that uses puts to print string
- * Return: ends with 0
-*/
+/**
+    - main - A program that prints a line with puts function
+    - Return: 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like buiding a multilingual puzzle");
-
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

c480fb9d4850f775a9c77b309976408b32220f6a
Sheriff <acorlor2real@gmail.com>
2023-02-24 18:36:57 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_line - draws a straight line
+  * @n: the lenght of the line
+  *
+  * Return: void
+  */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (n)
+		{
+			putchar('');
+			n--;
+		}
+	}
+	_putchar('\n');
+}

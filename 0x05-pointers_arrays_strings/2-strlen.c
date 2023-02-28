830e78dd22783a93fea9f681456db0a7664f82b4
Sheriff <acorlor2real@gmail.com>
2023-02-28 15:24:30 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * _strlen - a function that returns the length of a string
+  * @s: a pointer
+  * Return: always 0 
+  */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}

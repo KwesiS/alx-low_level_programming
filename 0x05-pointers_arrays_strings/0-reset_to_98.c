054977e23a977f63884e84dd9ee4e33854ad1eef
Sheriff <acorlor2real@gmail.com>
2023-02-28 14:36:01 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include 'main.h'
+/**
+ *  main - a function that takes pointer to an int as parameter
+ *  Return: Always 0
+ */
+
+void reset_to_98(int *n);
+{
+	int i;
+
+	i = 98;
+	int *p;
+	*p = 98;
+	
+}        

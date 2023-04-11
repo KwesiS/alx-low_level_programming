e37a82cd64ab1bc3d400bd01b835c38b7e60898c
Sheriff <acorlor2real@gmail.com>
2023-04-11 14:53:26 -0700

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,48 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * reads text file - a function that reads and prints to POSIX
+ * read_textfile - a function that reads a text file and prints it
+ *                to POSIX standard output.
  *
- * @fd: filename
+ * @filename: is the file to read
+ * @letters: number of letters to read and print from file
  *
- * Return: num_written
- */
+ * Return: 0 if it fails or actual number of letters it could
+ *         read and print
+*/
 
-ssize_t read_textfile(const char *filename, size_t letters) 
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    if (filename == NULL) {
-        return 0;
-    }
+	int file;
+	ssize_t read_check, wcount;
+	char *buffer;
 
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        return 0;
-    }
+	if (filename == NULL)
+		return (0);
 
-    char *buffer = (char *) malloc(letters * sizeof(char));
-    if (buffer == NULL) {
-        close(fd);
-        return 0;
-    }
+	file = open(filename, O_RDONLY);
 
-    ssize_t num_read = read(fd, buffer, letters);
-    if (num_read == -1) {
-        close(fd);
-        free(buffer);
-        return 0;
-    }
+	if (file == -1)
+		return (0);
 
-    ssize_t num_written = write(STDOUT_FILENO, buffer, num_read);
-    if (num_written == -1 || num_written != num_read) {
-        close(fd);
-        free(buffer);
-        return 0;
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    close(fd);
-    free(buffer);
-    return num_written;
+	read_check = read(file, buffer, letters);
+	if (read_check == -1)
+		return (0);
+
+	wcount = write(STDOUT_FILENO, buffer, read_check);
+	if (wcount == -1 || read_check != wcount)
+		return (0);
+
+	free(buffer);
+
+	close(file);
+
+	return (wcount);
 }

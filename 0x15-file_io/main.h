297aa7206a66a380663fd96a7a00b73150a48230
Sheriff <acorlor2real@gmail.com>
2023-04-11 15:20:16 -0700

    create main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,14 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-/*
- *create main header file 
- *list prototypes of all functions
- */
 
-
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
